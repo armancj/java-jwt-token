6dd51004bcb2e6c4f125ef6b0a7791b138ce6234
mandi <armancanjer@gmail.com>
2024-06-18 14:37:59 -0400

    feat(): implements claims json web token

--- src/main/java/com/gdai/security/config/JwtService.java
+++ src/main/java/com/gdai/security/config/JwtService.java
@@ -1,7 +1,26 @@
 package com.gdai.security.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+import java.security.PublicKey;
+
 public class JwtService {
+    private static final String SECRET_KEY= "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
     public String extractUserName(String jwt) {
         return null;
+    }
+
+
+    private Claims extractAllClaims(String jwt) {
+        return Jwts.parser().verifyWith(getSignInKey()).build().parseSignedClaims(jwt).getPayload();
+    }
+
+    private SecretKey getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
